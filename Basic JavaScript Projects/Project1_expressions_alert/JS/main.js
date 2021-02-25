0d574f4f6c286a112ecb68564aeb55fd5e8c69f5
BradCrepps <77355802+bradcrepps@users.noreply.github.com>
2021-02-25 13:09:21 -0800

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,14 +1,21 @@
 var A = "This is a string";
 document.write(A);
 
+//Window pop up//
 var Popuptext = "This is a window alert";
 window.alert(Popuptext);
 document.write(' Brad said "You rock" ' + "Also more text<br>");
 
+//Concainating strings//
 var B = "Concatenated" + "String<br>"
 document.write(B)
 
-var Family = "The Crepps", son2="Brad", daughter="Amanda", son1="Tony";
+//Family Variables//
+var Family = "The Crepps", son2="Brad<br>", daughter="Amanda<br>", son1="Tony<br>";
+
+//Font-Color for family variable//
 var daughter = daughter.fontcolor("green");
 document.write(daughter);
 
+//Expression example//
+document.write(3 + 3);
