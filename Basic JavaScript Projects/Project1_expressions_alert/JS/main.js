bcc9ea5fdbed24b44f0823aae88df55ccd57fb86
BradCrepps <77355802+bradcrepps@users.noreply.github.com>
2021-02-25 12:13:15 -0800

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,1 +1,14 @@
-alert("Hello, World!");
+var A = "This is a string";
+document.write(A);
+
+var Popuptext = "This is a window alert";
+window.alert(Popuptext);
+document.write(' Brad said "You rock" ' + "Also more text<br>");
+
+var B = "Concatenated" + "String<br>"
+document.write(B)
+
+var Family = "The Crepps", son2="Brad", daughter="Amanda", son1="Tony";
+var daughter = daughter.fontcolor("green");
+document.write(daughter);
+
