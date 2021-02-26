9e9d0f8b7c54d08181a4bddc699f2c50f6944662
BradCrepps <77355802+bradcrepps@users.noreply.github.com>
2021-02-25 22:55:33 -0800

    .

---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,31 @@
+//Addition function//
+function addition_function() {
+    var addition = 1+1;
+    document.getElementById("Math_addition").innerHTML= addition;
+}
+
+//Subtraction function//
+function subtraction_function() {
+    var Subtraction = 6-3;
+    document.getElementById("Math_subtraction").innerHTML= Subtraction;
+}
+
+//Multiplication function//
+function multiplication_function() {
+    var Multiplication = 5*4;
+    document.getElementById("Math_multiplication").innerHTML= Multiplication;
+}
+
+//Divison function//
+function division_function() {
+    var Divison = 1000/5;
+    document.getElementById("Math_divison").innerHTML= Divison;
+}
+
+//Multiple mathmatic expressions at once//
+function more_Math() {
+    var simple_Math = (1+2)*10/2-5;
+    document.getElementById("Math_simple").innerHTML= simple_Math;
+
+}
+
