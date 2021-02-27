6f9265fece5bd7f99bbf43780ddc4a949fc09927
BradCrepps <77355802+bradcrepps@users.noreply.github.com>
2021-02-26 21:58:19 -0800

    var symbols from equal to colon

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,9 +1,9 @@
 //Coffee Dictionary
 function coffee_drinks() {
     var espresso = {
-        roast="Dark",
-        flavor="Rich",
-        milk="None"
+        roast:"Dark",
+        flavor:"Rich",
+        milk:"None"
     };
-    document.getElementById("Coffee_Dictionary").innerHTML = espresso.flavor;
+    document.getElementById("Coffee_Dictionary").innerHTML= espresso.flavor;
 }
