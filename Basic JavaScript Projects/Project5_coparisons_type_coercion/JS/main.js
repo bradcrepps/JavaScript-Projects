2ded22ec76f2763c9f5689d35b4e9afad3818b00
BradCrepps <77355802+bradcrepps@users.noreply.github.com>
2021-03-11 12:38:33 -0800

    Project 5

---
+++ Basic JavaScript Projects/Project5_coparisons_type_coercion/JS/main.js
@@ -0,0 +1,12 @@
+document.write("10" + 5);
+
+//Positive infinity with variable
+function positive_infinity() {
+    var pos_inf;
+    document.write(2E310);
+    document.getElementById("plus").innerHTML = pos_inf;
+}
+//Negative infinity without variable
+function negative_infinity() {
+    document.getElementById("minus").innerHTML = document.write(-3E310);
+}
