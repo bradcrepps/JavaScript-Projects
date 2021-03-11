d6ce836d1554b42e0ce362db379dbbd1926a2dd4
BradCrepps <77355802+bradcrepps@users.noreply.github.com>
2021-03-11 12:45:03 -0800

    Update main.js

--- Basic JavaScript Projects/Project5_coparisons_type_coercion/JS/main.js
+++ Basic JavaScript Projects/Project5_coparisons_type_coercion/JS/main.js
@@ -10,3 +10,12 @@ function positive_infinity() {
 function negative_infinity() {
     document.getElementById("minus").innerHTML = document.write(-3E310);
 }
+
+//true boolean
+document.write(10 < 2);
+//false boolean
+document.write(10<2);
+//console log equation
+console.log(4+4);
+//false in the console log
+console.log(12<4);
