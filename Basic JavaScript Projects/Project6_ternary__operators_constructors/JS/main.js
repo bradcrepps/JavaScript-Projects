566914c831dafdca7dc150e4ce0feb4fed416d48
BradCrepps <77355802+bradcrepps@users.noreply.github.com>
2021-03-23 16:30:59 -0700

    Project6

---
+++ Basic JavaScript Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -0,0 +1,29 @@
+function Ride_Function() {
+    var Height, Can_ride;
+    Height = document.getElementById("Height").value;
+    Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
+    document.getElementById("Ride").innerHTML = Can_ride + " to ride.";
+}
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Subaru", "Forrester", 2021, "Navy")
+var Erich = new Vehicle("Ford", "GT", 2006, "White with Blue stripe")
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML= "Erich drives a " + Erich.Vehicle_Color + "-colored " + Erich.Vehicle_Model + " manufactured in " +Erich.Vehicle_Year;
+}
+
+function count_Function() {
+    document.getElementById("Nested_Function").innerHTML=Count();
+    function Count() {
+        var Starting_point= 9;
+        function Plus_one() {Starting_point += 1;}
+        Plus_one();
+        return Starting_point;
+    }
+}
