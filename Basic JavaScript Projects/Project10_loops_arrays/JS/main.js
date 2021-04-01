8ea1076a387ff5511e2064e7883156dd6bec134d
BradCrepps <77355802+bradcrepps@users.noreply.github.com>
2021-03-31 18:11:39 -0700

    Project 10 final

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -57,4 +57,4 @@ let car = {
         return "The car is a " + this.year + this.color+ this.make+ this.model;
     }
 };
-document.getElementById("Car_Object").innerHTML=car.description();
+document.getElementById("Car_Object").innerHTML = car.description();
